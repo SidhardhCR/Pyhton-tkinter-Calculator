6360801fe6b1e4ff87b34e903eb70ba6a53f9499
Sidhardh CR <sidhardhcr007@gmail.com>
2021-09-27 04:28:24 -0700

    MAin Functions Added

--- tkinter_calculator.py
+++ tkinter_calculator.py
@@ -1,6 +1,33 @@
 from tkinter import *
 
+# Globally declare the expressoin variable
+expression = ""
 
+
+# Function to update expression in the entry box
+def press(num):
+    global expression
+    expression = expression + str(num)
+
+#Function to evaluate the final expression
+def equal_press():
+
+    try:
+        global expression
+        #eval is used for evaluate the expression
+        total=str(eval(expression))
+
+
+        expression = ""
+
+    except:
+
+        expression = ""
+
+#Function to clear the entry box
+def clear():
+    global expression
+    expression = ""
 
 
 
