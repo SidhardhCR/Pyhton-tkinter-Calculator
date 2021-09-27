8ba179342e7b131da97a15bf82d29748466e5b45
Sidhardh CR <sidhardhcr007@gmail.com>
2021-09-27 04:21:53 -0700

    Problem solved

--- tkinter_calculator.py
+++ tkinter_calculator.py
@@ -1,27 +1,10 @@
 from tkinter import *
 
-#Simple Calculator
-#Declare expresson as global
- expression = ""
 
-def press(num):
-    global expression
 
-    expression = expression+str(num)
 
-#Function to evaluate the final expression
-def equal_press():
-    #Try and except statement is used for handling the error like zero division
-    try:
-        global expression
 
-        #eval is used for evaluate the expression
-        total = str(eval(expression))
+window=Tk()
 
-        expression = ""
 
-#Function to clear the entry box
-def clear():
-    global expression
-    expression = ""
-
+window.mainloop()
