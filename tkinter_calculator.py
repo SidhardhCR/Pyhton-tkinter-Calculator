7b5e316c0a1594483cfa482e1c21152532aba3dd
Sidhardh CR <sidhardhcr007@gmail.com>
2021-09-27 04:47:01 -0700

    Entry box added

--- tkinter_calculator.py
+++ tkinter_calculator.py
@@ -8,6 +8,7 @@ expression = ""
 def press(num):
     global expression
     expression = expression + str(num)
+    equation.set(expression)
 
 #Function to evaluate the final expression
 def equal_press():
@@ -16,12 +17,12 @@ def equal_press():
         global expression
         #eval is used for evaluate the expression
         total=str(eval(expression))
-
-
+        equation.set(total)
         expression = ""
 
     except:
 
+        equation.set("error")
         expression = ""
 
 #Function to clear the entry box
@@ -30,8 +31,23 @@ def clear():
     expression = ""
 
 
+#Create GUI window
 
 window=Tk()
+#Set background colour
 
+window.configure(background="gray")
+#Set title
+
+window.title('Simple Calculator')
+window.geometry('370x250')
+#StringVar is the variable class
+
+equation = StringVar()
+
+#Create the entry box
+
+input_field =Entry(window, textvariable=equation)
+input_field.grid(columnspan=8, ipadx=120)
 
 window.mainloop()
